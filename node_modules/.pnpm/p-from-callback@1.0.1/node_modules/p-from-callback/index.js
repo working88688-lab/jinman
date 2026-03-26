808ed2e06471877aaf95813387d8b4dd616efffc
heitu <heitu@gmail.com>
2026-03-26 23:57:16 +0800

    remove  node_modues

--- node_modules/.pnpm/p-from-callback@1.0.1/node_modules/p-from-callback/index.js
+++
@@ -1,9 +0,0 @@
-export default (fn, multi = false) => new Promise((resolve, reject) => {
-  fn(
-    (error, ...result) => (
-      error
-        ? reject(error)
-        : resolve(multi ? result : result[0])
-    ),
-  );
-});
